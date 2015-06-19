e5b524aa393cfffdb78cfb24bd3b20c189f0802a
dweinberger <daniel.weinberger@bitmovin.net>
2015-06-19 15:32:22 +0200

    added grunt as build tool and jasmine for tests

---
+++ Gruntfile.js
@@ -0,0 +1,69 @@
+/****************************************************************************
+ * Copyright (C) 2015, bitmovin GmbH, All Rights Reserved
+ *
+ * Created on: 6/19/15
+ * Author: Reinhard Grandl   <reinhard.grandl@bitmovin.net>
+ *         Daniel Weinberger <daniel.weinberger@bitmovin.net>
+ *
+ * This source code and its use and distribution, is subject to the terms
+ * and conditions of the applicable license agreement.
+ ****************************************************************************/
+
+module.exports = function(grunt) {
+  var header = '/****************************************************************************\n' +
+    '* Copyright (C) 2015, bitmovin GmbH, All Rights Reserved\n' +
+    '*\n' +
+    '* This source code and its use and distribution, is subject to the terms\n' +
+    '* and conditions of the applicable license agreement.\n' +
+    '****************************************************************************/\n';
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    concat: {
+      options: {
+        separator: ';\n',
+        banner: header + '\n(function(global) {\n',
+        footer: 'global.BitcodinApi = BitcodinApi;\n})(this);',
+        stripBanners: true
+      },
+      debug: {
+        src: 'src/**/*.js',
+        dest: 'debug/<%= pkg.name %>.js'
+      }
+    },
+    watch: {
+      local: {
+        files: [
+          'src/**/*.*',
+          'test/**/*.*',
+          '!node_modules/**',
+          '!_SpecRunner.html'
+        ],
+        options: {
+          livereload: true
+        },
+        tasks: ['concat', 'jasmine:app']
+      }
+    },
+    jasmine: {
+      app: {
+        src: [
+          'src/**/*.js'
+        ],
+        options: {
+          specs: 'test/**/*Spec.js',
+          outfile: 'test/SpecRunner.html',
+          keepRunner: true
+        }
+      }
+    }
+  });
+
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-jasmine');
+
+  grunt.registerTask('test', ['concat:debug', 'jasmine:app']);
+};
